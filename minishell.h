e6f10e867bd0b3f4fbcf24e8b238c56696483c86
msilva-c <madalena.morais888@gmail.com>
2024-11-22 04:41:55 +0000

    first merge

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marianamestre <marianamestre@student.42    +#+  +:+       +#+        */
+/*   By: msilva-c <msilva-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:57:32 by marianamest       #+#    #+#             */
-/*   Updated: 2024/11/11 20:03:36 by marianamest      ###   ########.fr       */
+/*   Updated: 2024/11/22 04:37:29 by msilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,10 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <unistd.h>
+# include <stdbool.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
 
 /* error handling */
 void	print_error(const char *message);
@@ -66,5 +70,60 @@ void	exec_exit(char **args);
 /* helper functions */
 void	my_strcpy(char *dest, const char *src);
 void	my_strcat(char *dest, const char *src);
+
+/* madalena */
+# define MAXLINE 1024
+
+typedef struct s_token
+{
+	char		*content;
+	int 		type;
+	struct s_token	*next;
+	struct s_token	*prev;
+}			t_token;
+
+typedef struct s_msh
+{
+	char		*line;
+	int		exit;
+	int		ret;
+	t_token **lst_head;
+	char **env;
+}			t_msh;
+
+// types for node struct
+#define CMD 1
+#define PIPE 2
+#define REDIR 3
+#define QUOTE 4
+#define ENVVAR 5
+#define STR 6
+
+/* env_utils.c */
+char **copy_matrix(char **src);
+
+/* init.c */
+t_msh    *init_all(t_msh *msh);
+
+/* free.c */
+void   free_and_exit(t_msh *msh);
+void	ft_free_matrix(char **matrix);
+
+/* lexer.c */
+int     findtype(char *s);
+t_token *tokenize(char *str, int start, int wdlen);
+int add_node(t_token **lst_head, char *line, int i, int j);
+int strchr_wdlen(const char *s, int c);
+void split_cmds(char *line, int i, int j, t_token **lst_head);
+void new_lexer(char *cmdline, t_token **lst_head);
+void	final_lexer(char *cmdline, t_token **lst_head);
+
+/* lst_utils.c */
+char	*substr_new(char const *s, unsigned int start, size_t len);
+void	ft_tknclear(t_token **lst);
+t_token	*newtoken(char *content);
+t_token	*ft_tknlast(t_token *lst);
+void	ft_tknadd_back(t_token **lst, t_token *newnode);
+int	quote_handler(char *cmdline, int i, t_token **lst_head);
 
 #endif
---
+++ src/parser/init.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msilva-c <msilva-c@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 18:18:18 by msilva-c          #+#    #+#             */
+/*   Updated: 2024/11/22 03:15:40 by msilva-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header.h"
+
+t_msh    *init_all(t_msh *msh)
+{
+    msh->line = (char *)malloc(sizeof(char));
+    if (!msh->line)
+        return (NULL);
+    msh->exit = 0;
+    msh->ret = 0;
+    msh->lst_head = (t_token **)malloc(sizeof(t_token *));
+    if (!msh->lst_head)
+        return (NULL);
+    //acho que a lista está mal iniciada, tem de ser sizeof(t_token) * nr de tokens a serem criados.
+    *(msh->lst_head) = NULL;
+    msh->env = NULL;
+    return (msh);
+}
