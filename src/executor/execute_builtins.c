6af96075746d0ed62207fb57ffb9fa71f5c821da
mariana <103138000+nanabv@users.noreply.github.com>
2025-03-18 11:04:26 +0000

    exec_builtins

--- src/executor/execute_builtins.c
+++ src/executor/execute_builtins.c
@@ -6,31 +6,26 @@
 /*   By: marianamestre <marianamestre@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 16:58:48 by marianamest       #+#    #+#             */
-/*   Updated: 2025/03/18 10:44:11 by marianamest      ###   ########.fr       */
+/*   Updated: 2025/03/18 11:04:12 by marianamest      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	execute_builtin(t_msh *msh, char **args, char ***export, int fd, char **envp)
+int	execute_builtin(t_msh *msh, char **args, char ***export, int fd)
 {
-	char **env;
-	
-	env = init_env_array(envp);
-	if (msh->exit == NULL)
-		msh->exit = 0;
 	if (fd == -1)
 		fd = STDOUT_FILENO;
 	if (ft_strcmp(args[0], "cd") == 0)
-		builtin_cd(args);
+		cd_command(msh->env, args);
 	else if (ft_strcmp(args[0], "export") == 0)
-		export_command(args, env, fd);
+		export_command(args, msh->env, fd);
 	else if (ft_strcmp(args[0], "unset") == 0)
-		exec_unset(env, *export, args, msh->exit);
+		exec_unset(msh->env, *export, args, msh->exit);
 	else if (ft_strcmp(args[0], "exit") == 0)
 		exec_exit(args);
 	else if (ft_strcmp(args[0], "env") == 0)
-		builtin_env(env, fd);
+		builtin_env(msh->env, fd);
 	else if (ft_strcmp(args[0], "pwd") == 0)
 		my_pwd(fd);
 	else if (ft_strcmp(args[0], "echo") == 0)
