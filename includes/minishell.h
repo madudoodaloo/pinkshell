2ec1815ad1b5db9b3fca41ea96a3df96c94b722d
msilva-c <madalena.morais888@gmail.com>
2025-03-19 18:31:14 +0000

    mario

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marianamestre <marianamestre@student.42    +#+  +:+       +#+        */
+/*   By: msilva-c <msilva-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:57:32 by marianamest       #+#    #+#             */
-/*   Updated: 2025/03/19 15:17:15 by marianamest      ###   ########.fr       */
+/*   Updated: 2025/03/19 18:22:14 by msilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,21 +44,27 @@
 # define R_IN 5
 # define HERE_DOC 6
 
-// void *struct - rever!!!  Placeholder for resources to free
 // rever se ainda vai ser preciso dps da ft_dup_env - Environment variables
-typedef struct s_data
+
+typedef struct s_exec
 {
-	bool			is_heredoc;
-	void			*strut;
-	char			**envp;
-	struct s_msh	*msh;
-}					t_data;
+	char	**redir_in;
+	char 	**redir_out;
+	char 	**args;
+	int		nr_cmds; //pipes - 1
+	int		pipe_fd[2];
+	int		in_fd; //vai ler
+	int		out_fd; //vai escrever
+	bool	is_heredoc;
+	char	**envp; // manter a t_env e arranjar uma função
+	bool	cmd_invalid;
+}					t_exec;
 
 typedef struct s_env
 {
-	char			*var;// PATH=usr/folder
-	char			*var_name;// PATH
-	char			*var_value;// usr/folder/
+	char			*var;
+	char			*var_name;
+	char			*var_value;
 	int				valid;
 	struct s_env	*next;
 }					t_env;
@@ -72,23 +78,6 @@ typedef struct s_token
 	struct s_token	*prev;
 }					t_token;
 
-typedef struct s_exec
-{
-	int				pid;
-	int				pipe[2];
-	int				has_doc;
-	int				doc_pipe[2];
-	int				out_fd;
-	int				in_fd;
-	bool			last_child;
-	bool			bad_command;
-	char			*path;
-	char			**red_out;
-	char			**red_in;
-	char			**cmd;
-	struct s_pipex	*next;
-	struct s_pipex	*previous;
-}					t_exec;
 
 // rever: criar uma ft que transforma linked list em char **
 typedef struct s_msh
@@ -99,7 +88,6 @@ typedef struct s_msh
 	t_env			*env;
 	t_token			*tokens;
 	t_exec			*exec;
-	t_data			*data;
 	int				exit;
 }					t_msh;
 
--- src/executor/init_exec.c
+++ src/executor/init_exec.c
@@ -3,18 +3,92 @@
 /*                                                        :::      ::::::::   */
 /*   init_exec.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marianamestre <marianamestre@student.42    +#+  +:+       +#+        */
+/*   By: msilva-c <msilva-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 20:08:55 by msilva-c          #+#    #+#             */
-/*   Updated: 2025/03/19 15:22:29 by marianamest      ###   ########.fr       */
+/*   Updated: 2025/03/19 18:30:50 by msilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-/* rever */
-int	init_exec(void)
+int	count_pipes(t_token *tokens)
 {
+	int nr_pipes;
+// tenho de dar handle do cenário em que nãao há pipes - o exec tem de a mm mandar malloc the 1
+	nr_pipes = 1;
+	if (!tokens)
+		return (-1);
+	while (tokens)
+	{
+		if (tokens->type == PIPE)
+			nr_pipes++;
+		tokens = tokens->next;
+	}
+	return (nr_pipes);
+}
+
+char **add_to_matrix(char *content, char **args)
+{
+	if (!args)
+		args =
+}
+
+int tokens_to_exec(t_token *token, t_exec *exec)
+{
+	int i;
+	i = 0;
+	while (token)
+	{
+		if (token->type == CMD)
+				exec[i]->args = add_to_matrix(token->content, exec[i].args);
+		else if (token->type == PIPE)
+				;
+		else if (token->type == R_APP || token->type == R_OUT) //REVER meter um prefixo antes do file delimiter
+				;
+		else if (token->type == R_IN || token->type == HERE_DOC)
+				;
+		if (token->type == CMD)
+			token = token->next;
+		else if (token->type == PIPE)
+		{
+			token = token->next;
+			i++;
+		}
+		else
+			token = token->next->next;
+
+
+
+	}
+}
+
+
+t_exec *init_exec(t_token *tokens);
+{
+	t_exec	*exec;
+
+	if (!tokens)
+		return ERRADO;
+	int nr_pipes;
+	nr_pipes = count_pipes(tokens); //CHECK IF SYNTAX IS TRULLY WORKING
+	exec = (t_exec *)safe_malloc(sizeof(t_exec) * nr_pipes);
+	if (tokens_to_exec(tokens, exec) < 0)
+		ERRADO; //dar handle desta merda
+	//dar free dos tokens todos;
+	return (exec);
+}
+
+
+/* rever */
+int	set_exec(void)
+{
+	t_token *tokens;
+	t_exec	*exec;
+
+	tokens = msh()->tokens;
+	exec = init_exec(tokens);
+
 	return (-1);
 }
 
